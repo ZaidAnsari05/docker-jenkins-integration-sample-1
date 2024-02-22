2a74c5eb0ef43714255ec14338a2852394aae40e
ZaidAnsari05 <zaidbingufran05>
2024-02-22 12:31:59 +0530

    first commit

---
+++ src/main/java/com/javatechie1/docker_jenkins_integration_sample_1/App.java
@@ -0,0 +1,13 @@
+package com.javatechie1.docker_jenkins_integration_sample_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
